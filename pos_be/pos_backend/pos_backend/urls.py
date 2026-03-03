dca908a831bfc3144042488e5943c319d1dfd756
Anurag Gupta <anurag_gupta@cargill.com>
2026-03-04 02:07:53 +0530

    changes for podman deployment

--- pos_be/pos_backend/pos_backend/urls.py
+++ pos_be/pos_backend/pos_backend/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 admin.site.site_header = os.getenv('DJANGO_ADMIN_SITE_HEADER', 'Django administration')
@@ -27,3 +28,4 @@ urlpatterns = [
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
